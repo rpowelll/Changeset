ddd715511acc588dfbabf4e0b306cdcd9191b8e1
rpowelll <rhys@rpowell.me>
2016-04-13 21:36:50 +1000

    No longer store the values for edits
    This makes the library less generally useful, but in the case where the
    intention is to generate table or collection view updates, performance is
    drastically improved

--- Changeset/Changeset.swift
+++ Changeset/Changeset.swift
@@ -4,27 +4,23 @@
 //
 
 /// Defines an atomic edit.
-/// - seealso: Note on `EditOperation`.
-public struct Edit<T: Equatable> {
-	public let operation: EditOperation
-	public let value: T
+/// - seealso: Note on `Operation`.
+public struct Edit {
+	public enum Operation {
+		case Insertion
+		case Deletion
+		case Substitution
+		case Move(origin: Int)
+	}
+	
+	public let operation: Operation
 	public let destination: Int
 	
 	// Define initializer so that we don't have to add the `operation` label.
-	public init(_ operation: EditOperation, value: T, destination: Int) {
+	public init(_ operation: Operation, destination: Int) {
 		self.operation = operation
-		self.value = value
 		self.destination = destination
 	}
-}
-
-/// Defines the type of an `Edit`.
-/// - note: I would have liked to make it an `Edit.Operation` subtype, but that's currently not allowed inside a generic type.
-public enum EditOperation {
-	case Insertion
-	case Deletion
-	case Substitution
-	case Move(origin: Int)
 }
 
 /// A `Changeset` is a way to describe the edits required to go from one set of data to another.
@@ -45,7 +41,7 @@ public struct Changeset<T: CollectionType where T.Generator.Element: Equatable, 
 	/// The edit steps required to go from `self.origin` to `self.destination`.
 	/// - note: I would have liked to make this `lazy`, but that would prohibit users from using constant `Changeset` values.
 	/// - seealso: [Lazy Properties in Structs](http://oleb.net/blog/2015/12/lazy-properties-in-structs-swift/) by [Ole Begemann](https://twitter.com/olebegemann).
-	public let edits: [Edit<T.Generator.Element>]
+	public let edits: [Edit]
 	
 	public init(source origin: T, target destination: T) {
 		self.origin = origin
@@ -67,25 +63,25 @@ public struct Changeset<T: CollectionType where T.Generator.Element: Equatable, 
 	///
 	/// - returns: An array of `Edit` elements.
 	/// The number of steps is then the `count` of elements.
-	public static func editDistance(source s: T, target t: T) -> [Edit<T.Generator.Element>] {
+	public static func editDistance(source s: T, target t: T) -> [Edit] {
 		
 		let m = s.count
 		let n = t.count
 		
 		// Fill first row and column of insertions and deletions.
 		
-		var d: [[[Edit<T.Generator.Element>]]] = Array(count: m + 1, repeatedValue: Array(count: n + 1, repeatedValue: []))
+		var d: [[[Edit]]] = Array(count: m + 1, repeatedValue: Array(count: n + 1, repeatedValue: []))
 		
-		var edits = [Edit<T.Generator.Element>]()
+		var edits = [Edit]()
 		for (row, element) in s.enumerate() {
-			let deletion = Edit(.Deletion, value: element, destination: row)
+			let deletion = Edit(.Deletion, destination: row)
 			edits.append(deletion)
 			d[row + 1][0] = edits
 		}
 		
 		edits.removeAll()
 		for (col, element) in t.enumerate() {
-			let insertion = Edit(.Insertion, value: element, destination: col)
+			let insertion = Edit(.Insertion, destination: col)
 			edits.append(insertion)
 			d[0][col + 1] = edits
 		}
@@ -114,15 +110,15 @@ public struct Changeset<T: CollectionType where T.Generator.Element: Equatable, 
 					
 					let minimumCount = min(del.count, ins.count, sub.count)
 					if del.count == minimumCount {
-						let deletion = Edit(.Deletion, value: s[sx], destination: i - 1)
+						let deletion = Edit(.Deletion, destination: i - 1)
 						del.append(deletion)
 						d[i][j] = del
 					} else if ins.count == minimumCount {
-						let insertion = Edit(.Insertion, value: t[tx], destination: j - 1)
+						let insertion = Edit(.Insertion, destination: j - 1)
 						ins.append(insertion)
 						d[i][j] = ins
 					} else {
-						let substitution = Edit(.Substitution, value: t[tx], destination: i - 1)
+						let substitution = Edit(.Substitution, destination: i - 1)
 						sub.append(substitution)
 						d[i][j] = sub
 					}
@@ -135,59 +131,14 @@ public struct Changeset<T: CollectionType where T.Generator.Element: Equatable, 
 		}
 		
 		// Convert deletion/insertion pairs of same element into moves.
-		return reducedEdits(d[m][n])
+		return d[m][n]
 	}
 }
 
-/// Returns an array where deletion/insertion pairs of the same element are replaced by `.Move` edits.
-private func reducedEdits<T: Equatable>(edits: [Edit<T>]) -> [Edit<T>] {
-	return edits.reduce([Edit<T>]()) {
-		(var reducedEdits, edit) in
-		
-		if let (move, index) = moveFromEdits(reducedEdits, deletionOrInsertion: edit), case .Move = move.operation {
-			reducedEdits.removeAtIndex(index)
-			reducedEdits.append(move)
-		} else {
-			reducedEdits.append(edit)
-		}
-		
-		return reducedEdits
-	}
-}
-
-/// Returns a potential move `Edit` based on an array of `Edit` elements and an `edit` to match up against.
-///
-/// If `edit` is a deletion or an insertion, and there is a matching inverse insertion/deletion with the same value in the array, a corresponding `.Move` edit is returned. 
-///
-/// As a convenience, the index of the matched edit into `edits` is returned as well.
-private func moveFromEdits<T: Equatable>(edits: [Edit<T>], deletionOrInsertion edit: Edit<T>) -> (move: Edit<T>, index: Int)? {
-	
-	switch edit.operation {
-	
-	case .Deletion:
-		if let insertionIndex = edits.indexOf({ (earlierEdit) -> Bool in
-			if case .Insertion = earlierEdit.operation where earlierEdit.value == edit.value { return true } else { return false }
-		}) {
-			return (Edit(.Move(origin: edit.destination), value: edit.value, destination: edits[insertionIndex].destination), insertionIndex)
-		}
-	
-	case .Insertion:
-		if let deletionIndex = edits.indexOf({ (earlierEdit) -> Bool in
-			if case .Deletion = earlierEdit.operation where earlierEdit.value == edit.value { return true } else { return false }
-		}) {
-			return (Edit(.Move(origin: edits[deletionIndex].destination), value: edit.value, destination: edit.destination), deletionIndex)
-		}
-		
-	default:
-		break
-	}
-	
-	return nil
-}
 
 extension Edit: Equatable {}
-public func ==<T: Equatable>(lhs: Edit<T>, rhs: Edit<T>) -> Bool {
-	guard lhs.destination == rhs.destination && lhs.value == rhs.value else { return false }
+public func ==(lhs: Edit, rhs: Edit) -> Bool {
+	guard lhs.destination == rhs.destination  else { return false }
 	switch (lhs.operation, rhs.operation) {
 	case (.Insertion, .Insertion), (.Deletion, .Deletion), (.Substitution, .Substitution):
 		return true
